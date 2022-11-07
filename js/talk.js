c295e6f98670347d975f8e6340dce54cfc99e87c
CCzy <54843671+Wzysaber@users.noreply.github.com>
2022-11-08 00:54:35 +0800

    Update talk.js

--- js/talk.js
+++ js/talk.js
@@ -3,12 +3,12 @@ new TypeIt("#lywry", {
         cursorSpeed: 1000,
         speed: 100
     })
-    .type("LY && WRY")
+    .type("小培是子溢最可爱的宝呀")
     .pause(2000)
     .delete(null, {
         delay: 500
     })
-    .type("感谢有你")
+    .type("我家小培今年必TM上岸！！！")
     .pause(3000)
     .go();
 
@@ -16,5 +16,5 @@ new TypeIt('#talkToWRY', {
     lifeLike: true,
     cursorSpeed: 1000,
     waitUntilVisible: true,
-    speed: 100
+    speed: 50
 }).go();
